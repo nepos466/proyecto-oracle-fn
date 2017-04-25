deeb421f4cebb7da9eb330e098166e779a7650f8
nepos466 <nepos466@gmail.com>
2017-04-25 18:31:48 -0500

    Update TestProcedimiento.java

--- src/java/model/TestProcedimiento.java
+++ src/java/model/TestProcedimiento.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 
 /**
  *
- * @author campitos
+ * @author IVAN NEPOMUCENO LEON
  */
 public class TestProcedimiento {
     public static void main(String args[])throws Exception{
