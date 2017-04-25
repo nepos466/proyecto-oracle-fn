b74c1e9886b37fe2455844e9ec69d22bd62c3464
nepos466 <nepos466@gmail.com>
2017-04-25 18:33:01 -0500

    Update TestProcedimiento.java

--- src/java/model/TestProcedimiento.java
+++ src/java/model/TestProcedimiento.java
@@ -1,4 +1,4 @@
-package model;
+Rpackage model;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
@@ -11,14 +11,14 @@ public class TestProcedimiento {
     public static void main(String args[])throws Exception{
         Conexion c=new Conexion();
         Connection con=c.conectarse("system","system");
-        CallableStatement callate=con.prepareCall("{call guardar_persona(?,?,?,?)}");
-        callate.registerOutParameter(1,java.sql.Types.INTEGER);
-        callate.setString(2,"Ivan");
-        callate.setString(3,"Nepomuceno");
-        callate.setInt(4,23);
+        CallableStatement ingresar=con.prepareCall("{call guardar_persona(?,?,?,?)}");
+        ingresar.registerOutParameter(1,java.sql.Types.INTEGER);
+        ingresar.setString(2,"Ivan");
+        ingresar.setString(3,"Nepomuceno");
+        ingresar.setInt(4,23);
       
-        callate.execute();
-        int pk=callate.getInt(1);
+        ingresar.execute();
+        int pk=ingresar.getInt(1);
         System.out.println("El id ingresado es:"+pk);
     }
     
